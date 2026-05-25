f64b6e20acb20169ec39d145ae11f4aa4cf45e6d
faizaHusna <faizatuulhusna@gmail.com>
2026-05-25 20:19:20 +0700

    Initial Commit - Aplikasi Katalog Dessert

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.praktikum_p12_p13"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+
+        buildFeatures {
+            viewBinding = true
+        }
+
+        defaultConfig {
+            applicationId = "com.example.praktikum_p12_p13"
+            minSdk = 24
+            targetSdk = 36
+            versionCode = 1
+            versionName = "1.0"
+
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+
+        buildTypes {
+            release {
+                isMinifyEnabled = false
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            }
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+    }
+
+    //noinspection WrongGradleMethod
+    dependencies {
+        implementation(libs.appcompat)
+        implementation(libs.material)
+        implementation(libs.activity)
+        implementation(libs.constraintlayout)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.ext.junit)
+        androidTestImplementation(libs.espresso.core)
+        implementation ("com.android.volley:volley:1.2.1")
+        implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+        implementation ("com.github.bumptech.glide:glide:4.16.0")
+        annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
+
+    }
+}
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+}
